3a2e2baeeec1b80f96140db4fd334b1168008d4d
kshitij khatri <kshitizkhatri81@gmail.com>
2026-01-07 13:19:50 +0545

    change in handle request function

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,30 @@
-// importing necessary modules 
 #![allow(unused_imports, unused_variables, dead_code)]
 use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener,TcpStream};
 use std::thread::spawn;
 
-fn handle_client(mut stream: TcpStream){
-    //this is a buffer to read data from the client 
-    let mut buffer = [0; 1024];
-    // this reads the data from stream and store it in buffer. excpect gives error with msg when stream.read fails
+
+fn handle_request(mut stream:TcpStream){
+    let mut buffer = [0;1024];
     stream.read(&mut buffer).expect("Error reading from stream");
-    // this line converts the data in the buffer into utf8 incoded string
     let request = String::from_utf8_lossy(&buffer[..]);
-    println!("Recieved request: {}", request);
-    // as_bytes converts the text into bytes 
-    let response = "hello world ".as_bytes();
-    stream.write(response).expect("failed to write response");
+    println!("Request recieved : {}",request);
+    let response = "hello world".as_bytes();
+    stream.write(response).expect("Error writing in stream");
 }
 
-
-fn main() {
-    //creating a TCP listner 
-    let listner = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to address");
-    println!("Server listining on 127.0.0.1:8000");
-    // loop all the incomming listner 
+fn main () {
+    let listner = TcpListener::bind("127.0.0.1:8080").expect("Failed to listen ");
+    println!("Server is listining on {:?}", listner);
     for stream in listner.incoming(){
-        match stream{
+        match stream {
             Ok(stream) => {
-                //spawn helps to create new threat and if the connections are many then it runs concurrently 
-                // using move because new threat must own the stream
-                std::thread::spawn( move || handle_client(stream));
+                spawn(move || handle_request(stream));
             }
             Err(e) => {
-                //macro to print a error message 
-                eprint!("Failed to estb connection {}",e );
-                // stderr - standard error system 
-                
+                eprint!("Error :{}", e);
             }
         }
     }
+
 }
